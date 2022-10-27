e0acb5c5e1d68216dcccaca10f8098b16053e498
Sharan Phadke <phadkesharan@gmail.com>
2022-10-27 18:13:34 +0530

    NEW: add order post request

--- app.js
+++ app.js
@@ -3,14 +3,30 @@ const loki = require('lokijs')
 const { v4: uuidv4 } = require('uuid');
 
 const PORT = 8000
-
 const app = express()
+app.use(express.json());
 
-const db = new loki('Customer.db')
-const users = new db.addCollection('users')
 
 app.post('/order', (req, res)=>{
-    const {location, n_people, dishes} = req.body;
+
+    console.log(req.body)
+
+    const {location, n_people, seat, dishes} = req.body;
+
+    let db = new loki('customer.db');
+    let users = db.addCollection('users');
+
+    users.insert({
+        id: uuidv4(),
+        location: location,
+        n: n_people,
+        seat: seat,
+        dishes: dishes
+    })
+
+    const results = users.find();
+    console.log(results)
+    res.sendStatus(200);
 
 })
 
