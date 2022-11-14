de00d22ca685fb38bfecadeec92e5da2d4d291ec
Sharan Phadke <phadkesharan@gmail.com>
2022-11-15 02:11:12 +0530

    NEW: debug

--- app.js
+++ app.js
@@ -9,12 +9,25 @@ app.use(express.json());
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
-  });
+});
 
-app.post('/order', (req, res)=>{
+const rows = [
+    { id: 1, DishName: 'Savor Party Bread', Price: 299 },
+    { id: 2, DishName: 'Fruit Charcuterie Board', Price: 359 },
+    { id: 3, DishName: 'Hot Spinach Artichoke Dip', Price: 499 },
+    { id: 4, DishName: 'Fried Prosciutto Tortellini', Price: 199 },
+    { id: 5, DishName: 'Newyork Cheese Cake', Price: 299 },
+    { id: 6, DishName: 'Lemon Meringue Pie', Price: 199 },
+    { id: 7, DishName: 'Warm bread pudding', Price: 449 },
+    { id: 8, DishName: 'Red Velvet Cake', Price: 999 },
+];
+
+
+
+app.post('/order', (req, res) => {
     console.log(req.body)
 
-    const {location, n_people, seat, dishes} = req.body;
+    const { location, n_people, seat, dishes } = req.body;
 
     let db = new loki('customer.db');
     let users = db.addCollection('users');
@@ -29,13 +42,17 @@ app.post('/order', (req, res)=>{
 
     const results = users.find();
     console.log(results)
+
+    results.dishes.forEach(i=>{
+        console.log(rows[i-1][DishName])
+    })
     res.sendStatus(200);
 
 })
 
 
-app.listen(PORT, (err)=>{
-    if(err)
+app.listen(PORT, (err) => {
+    if (err)
         console.log(err)
     console.log(`App running at port ${PORT}`)
 })
