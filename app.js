9d01f8398f729ea80afdbf629651d620ebe73a2b
Sharan Phadke <phadkesharan@gmail.com>
2022-11-15 02:02:44 +0530

    FIX: cors fix kindof

--- app.js
+++ app.js
@@ -6,9 +6,12 @@ const PORT = 8000
 const app = express()
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    next();
+  });
 
 app.post('/order', (req, res)=>{
-
     console.log(req.body)
 
     const {location, n_people, seat, dishes} = req.body;
